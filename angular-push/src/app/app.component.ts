eab84cb37a1659a87d95fa855c87314cc0bf1eab
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2021-03-05 12:47:53 +0530

    package.json fix

--- angular-push/src/app/app.component.ts
+++ angular-push/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { PushNotificationsService} from 'ng-push';
+import { PushNotificationsService} from 'ng-push';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Web push Notifications!';
-  
+  constructor( private _pushNotifications: PushNotificationsService ) {
+    this._pushNotifications.requestPermission();
+  }
+
+  notify(){ //our function to be called on click
+    let options = { //set options
+      body: "The truth is, I'am Iron Man!",
+      icon: "assets/images/ironman.png" //adding an icon
+    }
+     this._pushNotifications.create('Iron Man', options).subscribe( //creates a notification
+        res => console.log(res),
+        err => console.log(err)
+    );
+  }
 }
